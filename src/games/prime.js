44cee963e75ceae827dad2d8edda8bfb715a943e
Andrey-Ryabchikov <a_ryabchikov@inbox.ru>
2024-02-26 00:49:46 +0300

    Added The Prime game

---
+++ src/games/prime.js
@@ -0,0 +1,32 @@
+import { getRandomNumber, runGame } from '../index.js';
+
+const isPrime = (num) => {
+    if (num < 2) {
+      return false;
+    }
+  
+    for (let i = 2; i <= Math.sqrt(num); i += 1) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+  
+    return true;
+  };
+
+  const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+  const playPrime = () => {
+    const getQuestionAndAnswer = () => {
+      const question = getRandomNumber(100);
+      const correctAnswer = isPrime(question) ? 'yes' : 'no';
+      
+      return { question: `${question}`, answer: correctAnswer };
+    };
+  
+    runGame(gameRules, getQuestionAndAnswer);
+  };
+  
+  export default playPrime;
+  
+
